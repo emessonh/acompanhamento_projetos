9d10228ea3533600a09192755d7ff2af46f94b0d
emessonh <ehs4@aluno.ifal.edu.br>
2023-11-17 14:04:40 -0300

    página home funcionando

--- projetos/views.py
+++ projetos/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Projeto 
+from django.core.paginator import Paginator
 
 # Create your views here.
+
+def listagemProjetos(request):
+    projects_list = Projeto.objects.all().order_by('data_criacao')
+
+    paginator = Paginator(projects_list, 5)
+    page = request.GET.get('page')
+    projects = paginator.get_page(page)
+
+    return render(request, 'projetos/home.html', {'projetos':projects})
